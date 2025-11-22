b2b427f3ba9b1e593e4cd13ad3d7f81412259a0a
Muhammad Ismail <ismailarshad99699@gmail.com>
2025-11-22 15:41:13 +0500

    feat: add CI Pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello, World!");
+console.log("This is the index.js file.");
+console.log("I am learning DevOps using version control systems.");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting test.js file.....");
+setTimeout(() => console.log("Test file executed after 3 seconds"), 3000);
+console.log;("Test Completed");
