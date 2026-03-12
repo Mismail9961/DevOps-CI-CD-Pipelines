5d4c2b012f668dd6797502500299936c5ca8ae14
Ismail <ismailarshad99699@gmail.com>
2026-03-13 01:24:54 +0500

    chore: remove obsolete CI/CD pipeline and related files

--- index.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello, World!");
-console.log("This is the index.js file.");
-console.log("I am learning DevOps using version control systems.");
--- test.js
+++
@@ -1,3 +0,0 @@
-console.log("Starting test.js file.....");
-setTimeout(() => console.log("Test file executed after 3 seconds"), 3000);
-console.log;("Test Completed");
