c114eae75c99077a0266cda76926fdffcb49ca57
Ismail <ismailarshad99699@gmail.com>
2026-03-19 20:33:13 +0500

    New Chnges For Web to run

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log('Hello, World!');
 console.log('I am learning CI/CD with GitHub Actions.');
+console.log('This is a simple Node.js application running in a Docker container.');
+console.log('Thank you for visiting my application!');
