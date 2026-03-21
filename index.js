e358c1cde425440a3b95fd551e90b485c96fe670
Ismail <ismailarshad99699@gmail.com>
2026-03-21 23:09:19 +0500

    Updated the outedated docker  file

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 console.log('Hello, World!');
 console.log('I am learning CI/CD with GitHub Actions.');
 console.log('This is a simple Node.js application running in a Docker container.');
-console.log('Thank you for visiting my application!');
